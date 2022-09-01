d2332cc2aa2c76763ade8b6ce6969e632de63eb9
Adam <adamgrharvey@gmail.com>
2022-09-01 16:58:01 -0700

    update to application controller to support sessions

--- backend/typing/app/controllers/application_controller.rb
+++ backend/typing/app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+  protect_from_forgery with: :exception
+
+  private
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perform that action."
+      redirect_to '/'
+    end
+  end   
 end
