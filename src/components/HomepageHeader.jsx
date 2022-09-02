8c71756ef394ec72483230635c0df7943bfd1525
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-09-02 14:27:51 -0700

    test btn for setting username

--- src/components/HomepageHeader.jsx
+++ src/components/HomepageHeader.jsx
@@ -7,7 +7,8 @@ const HomepageHeader = () => {
   return (
     <>
       <h1 className="bg-blood-red text-3xl font-bold underline">HomepageHeader</h1>
-      {user.name ? <h1> User has a name</h1> : <h1>User has no name</h1>}
+      {user.name ? <h1> User has a of {user.name}</h1> : <h1>User has no name</h1>}
+      <button onClick={() => setUser({ name: 'Curtis' })}>CLICK ME</button>
     </>
   );
 };
