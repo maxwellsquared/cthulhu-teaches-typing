2853fc2d79451bb5deb58e540ba981074631ba11
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-09-02 16:39:18 -0700

    save

--- README.md
+++ README.md
@@ -14,6 +14,15 @@ rails db:drop
 rails db: setup
 ```
 
+Users in the database:
+```
+ id |     name      |          email           | password | profile_picture |         created_at         |         updated_at
+----+---------------+--------------------------+----------+-----------------+----------------------------+----------------------------
+  1 | Adam Harvey   | adamgrharvey@gmail.com   | test     |                 | 2022-09-02 20:37:48.972712 | 2022-09-02 20:37:48.972712
+  2 | Curtis Warcup | curtis.gwarcup@gmail.com | test     |                 | 2022-09-02 20:37:48.974932 | 2022-09-02 20:37:48.974932
+  3 | Max Kuhn      | maxkuhn@gmail.com        | test     |                 | 2022-09-02 20:37:48.976708 | 2022-09-02 20:37:48.976708
+```
+
 
 ## Routes to know
 
@@ -24,3 +33,4 @@ In terminal
 curl POST http://localhost:3000/login -H 'Content-Type: application/json' -d '{"password":"test","email":"adamgrharvey@gmail.com"}' -v
 ```
 > Returns the user object if the user is in the db
+
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,74 +1,93 @@
-import React, { Fragment, useEffect, useState } from "react";
-import axios from "axios";
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
 import { UserContext } from '../helpers/context';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 
 function Login() {
   const { user, setUser } = useContext(UserContext);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   console.log('user', user);
+
+  const loggedInUserRef = useRef(null);
+  console.log('loggedInUserRef', loggedInUserRef);
+
+  function sendRequest(email, password, user, setUser) {
+    let loginData = {
+      email: email,
+      password: password,
+    };
+
+    const successfullLogin = (response) => {
+      loggedInUserRef.current = response.data;
+      setUser(loggedInUserRef.current);
+    };
+
+    const postRequest = new Promise((resolve, reject) => {
+      axios
+        .post(`http://localhost:3000/login`, loginData, {
+          headers: {
+            'content-type': 'application/json',
+          },
+        })
+        .then((res) => {
+          console.log('res.data', res.data);
+          setUser({ name: res.data.name });
+          successfullLogin(res);
+          resolve(res.data);
+          // need to use useRef to update the state of the user
+        })
+        .catch((err) => {
+          console.log(err);
+          reject(err);
+        });
+    });
+
+    console.log(postRequest);
+  }
+
   return (
     <Fragment>
-      <form onSubmit={event => {
-        event.preventDefault();
-      }} autoComplete="off">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+        autoComplete="off"
+      >
         <input
           value={email}
           type="text"
           placeholder="Email"
-          onChange={(event) => { setEmail(event.target.value); }}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
         />
       </form>
       <div></div>
-      <form onSubmit={event => {
-        event.preventDefault();
-      }} autoComplete="off">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+        autoComplete="off"
+      >
         <input
           value={password}
           type="password"
           placeholder="Password"
-          onChange={(event) => { setPassword(event.target.value); }}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
         />
       </form>
       <button
         onClick={() => {
           sendRequest(email, password, user, setUser);
-          
         }}
       >
         Login
       </button>
     </Fragment>
-  )
+  );
 }
 
-function sendRequest(email, password, user, setUser) {
-
-  let loginData = {
-    email: email,
-    password: password
-  }
-
-  const postRequest = new Promise((resolve, reject) => {
-    axios.post(`http://localhost:3000/login`, loginData, {
-      headers: {
-        'content-type': 'application/json'
-      }
-    })
-      .then(res => {
-        console.log(res.data)
-        setUser({ name: res.data.name })
-        resolve(res.data)
-      })
-      .catch(err => {
-        console.log(err)
-        reject(err)
-      })
-  })
-  
-  console.log(postRequest);
-
-}
-
-export default Login
+export default Login;
