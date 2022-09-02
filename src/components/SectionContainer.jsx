dc4a17af71d8a23caf633c3978f5f108918953f2
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-09-02 11:30:49 -0700

    added tailwind config

---
+++ src/components/SectionContainer.jsx
@@ -0,0 +1,5 @@
+export default function SectionContainer({ children }) {
+  return <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>;
+}
+
+// will wrap all the content in the main section
--- src/components/Span.jsx
+++ src/components/Span.jsx
@@ -4,9 +4,19 @@ const Span = ({ index, word, color, isCurrentWord, isLast, textDecoration }) => 
   };
   return (
     <span key={index} style={style}>
-      {word} {isLast ? '' : ','}
+      {word} {isLast ? '' : ' '}
     </span>
   );
 };
 
 export default Span;
+
+
+// scrap logging in and creating an account
+// for demo day...
+// literally create 3 seeds users, give them username and password
+// have login be a lookup for username and password mathcing
+// react - useContext to store the user. so it can survice re renders
+// a user can have many keybaords
+// name and id
+// seed data - 3 users, 3 keyboards
