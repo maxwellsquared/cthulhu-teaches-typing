763a7560fcdb71212d80622f1e25d314874de28b
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-09-02 19:39:01 -0700

    Removed UserContext from login

--- src/App.js
+++ src/App.js
@@ -18,14 +18,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
-        <Route
-          path="/login/*"
-          element={
-            <UserContext.Provider value={{ user, setUser }}>
-              <Login />
-            </UserContext.Provider>
-          }
-        />
+        <Route path="login" element={<Login />} />
       </Routes>
     </UserContext.Provider>
   );
