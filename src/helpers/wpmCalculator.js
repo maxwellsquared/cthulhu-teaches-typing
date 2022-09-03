24bdd3dfc1af0ed35593719bb81097e459686006
Adam <adamgrharvey@gmail.com>
2022-09-03 16:41:36 -0700

    helper function to take totalchar inputs, and count of mistakes. returns object with keystroke data and rounded WPM.

--- src/helpers/wpmCalculator.js
+++ src/helpers/wpmCalculator.js
@@ -1,3 +1,13 @@
-const wpmCalculator = function() {
+const wpmCalculator = function(totalChars, mistakes) {
+  let time = 1;
+  let correctChars = totalChars - mistakes;
+
+  let wpmObject = {
+    correctKeystrokes: correctChars / 5,
+    incorrectKeystrokes: mistakes / 5,
+    WPM: Math.round(correctKeystrokes) / time
+  }
+
+  return wpmObject;
   
 }
