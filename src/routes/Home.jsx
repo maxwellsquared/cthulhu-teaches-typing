e856cd074dbc24afe354fc75279a00f97044596d
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-09-03 08:43:41 -0700

    Small change to index.js for react 18.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,15 @@
 import LayoutWrapper from '../components/LayoutWrapper';
 import TypingField from '../components/TypingField';
+import { useContext } from 'react';
+import { UserContext } from '../helpers/context';
 
 // this should be the main page, displayed when URL is '/'
 const Home = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <LayoutWrapper>
-      <TypingField />
+      <TypingField userRef={user} />
     </LayoutWrapper>
   );
 };
