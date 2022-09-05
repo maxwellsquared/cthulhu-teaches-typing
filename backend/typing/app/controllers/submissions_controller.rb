3691c7bb74bd8902f1352b22b62fb6b4b26742d9
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-09-05 16:26:33 -0700

    backend add api/user/:id

--- backend/typing/app/controllers/submissions_controller.rb
+++ backend/typing/app/controllers/submissions_controller.rb
@@ -2,19 +2,28 @@ class SubmissionsController < ApplicationController
   def new
     @submission = Submission.new
   end
+
   def show # /api/leaderboard
-    @submissions = Submission.joins(:user).select("submissions.*, users.name, users.id").order(wpm: :desc).limit(10)
+    @submissions = Submission.joins(:user).select('submissions.*, users.name, users.id').order(wpm: :desc).limit(10)
     render json: @submissions
   end
+
   def create
     @submission = Submission.new(submission_params)
     if @submission.save
       render json: @submission
     else
       render json: @submission.errors.full_messages
+    end
   end
+
+  def history
+    @submissions = Submission.where(user_id: params[:id]).order(created_at: :desc)
+    render json: @submissions
   end
+
   private
+
   def submission_params
     params.require(:submission).permit(:wpm, :user_id)
   end
@@ -22,3 +31,4 @@ end
 
 # curl POST http://localhost:3000/submit -H 'Content-Type: application/json' -d '{"wpm":100,"user_id": 1}' -v
 # curl GET http://localhost:3000/api/leaderboard -H 'Content-Type: application/json' -v
+# curl GET http://localhost:3000/api/user/1 -H 'Content-Type: application/json' -v
