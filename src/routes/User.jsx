149a65c82b76764c97491a3e80bbaa7890ac1e09
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-09-05 16:57:56 -0700

    returns user stats

--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -1,25 +1,47 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../helpers/context';
+import axios from 'axios';
 
 const User = () => {
   const { user } = useContext(UserContext);
 
+  const [userStats, setUserStats] = useState();
+  const [error, setError] = useState();
 
-  return (
-    <>
-      {user ? (
-        <div>
-          <h1>{user.name}</h1>
-          <h1>{user.email}</h1>
-        </div>
-      ) : (
-        <div>
-          <h1>Oops!</h1>
-          <p>You must be logged in to view this page.</p>
-        </div>
-      )}
-    </>
-  );
+  // axios get request to get user data
+  const getUserData = () => {
+    axios
+      .get(`http://localhost:3000/api/user/${user.id}`)
+      .then((res) => {
+        console.log('Success: user data retrieved');
+        setUserStats(res.data);
+      })
+      .catch((err) => {
+        console.log('Error has occurred');
+        console.log(err);
+        setError('Error: user data could not be retrieved');
+      });
+  };
+
+  console.log(userStats);
+
+  // useEffect to only load user data once
+  useEffect(() => {
+    if (user) {
+      getUserData();
+    }
+  }, []);
+
+  const statsArr = userStats?.map((stat) => {
+    return (
+      <div key={stat.id} className="font-mono text-blood-red">
+        <p>WPM: {stat.wpm}</p>
+        <p>Date: {stat.created_at}</p>
+      </div>
+    );
+  });
+
+  return <>{statsArr}</>;
 };
 
 export default User;
