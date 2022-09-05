99396c86126d403210a28cd11a2fede7129a4fd0
Adam <adamgrharvey@gmail.com>
2022-09-05 10:29:46 -0700

    adds route and logic for backend replying with submission data in descending order

--- backend/typing/app/controllers/submissions_controller.rb
+++ backend/typing/app/controllers/submissions_controller.rb
@@ -2,6 +2,10 @@ class SubmissionsController < ApplicationController
   def new
     @submission = Submission.new
   end
+  def show
+    @submissions = Submission.order(wpm: :desc)
+    render json: @submissions
+  end
   def create
     @submission = Submission.new(submission_params)
     if @submission.save
@@ -17,3 +21,4 @@ class SubmissionsController < ApplicationController
 end
 
 # curl POST http://localhost:3000/submit -H 'Content-Type: application/json' -d '{"wpm":100,"users_id": 1}' -v
+# curl GET http://localhost:3000/leaderboard -H 'Content-Type: application/json' -v
