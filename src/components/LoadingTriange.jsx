49d37bf2070eb256968c08087c8f165d7eefbab6
Adam <adamgrharvey@gmail.com>
2022-09-05 11:45:46 -0700

    adds spinner package, and loading spinner to the leaderboard page

---
+++ src/components/LoadingTriange.jsx
@@ -0,0 +1,15 @@
+function LoadingTriange() {
+  return (
+    <Triangle
+      height="80"
+      width="80"
+      color="#4fa94d"
+      ariaLabel="triangle-loading"
+      wrapperStyle={{}}
+      wrapperClassName=""
+      visible={true}
+    />
+  );
+}
+
+export default LoadingTriange;
--- src/routes/Leaderboard.jsx
+++ src/routes/Leaderboard.jsx
@@ -1,5 +1,49 @@
+import axios from "axios";
+import { useState, CSSProperties } from "react";
+import BarLoader from "react-spinners/BarLoader";
+
+
+
 function Leaderboard() {
-  return <h1 className="mt-5">This is the Leaderboard</h1>;
+  const [loading, setLoading] = useState(true);
+
+  function sendRequest(setLoading) {
+    return (new Promise((resolve, reject) => {
+      axios
+        .get(`http://localhost:3000/api/leaderboard`, {
+          headers: {
+            'content-type': 'application/json',
+          },
+        })
+        .then((res) => {
+          // if server returns 200 (success)
+          if (res.status === 200) {
+          }
+          //setLoading(false);
+          resolve(res.data);
+
+        })
+        .catch((err) => {
+          console.log(err);
+          //setLoading(false);
+          reject(err);
+        });
+    }))
+
+
+  }
+  let leaderboardData;
+  let leaderboardPromise = sendRequest(setLoading);
+  leaderboardPromise.then((data) => {
+    leaderboardData = data;
+    console.log(leaderboardData);
+  })
+
+
+  return (
+    loading ? <BarLoader color={'#5118a7'}/> : <h1 className="mt-5">This is the Leaderboard</h1>
+  );
+
 }
 
 export default Leaderboard;
