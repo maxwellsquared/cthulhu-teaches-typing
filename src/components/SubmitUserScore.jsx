35d84b24597bd74d91737d9f0c5ef236fa3f9555
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-09-05 16:02:02 -0700

    Button text conditional depending on response from db

--- src/components/SubmitUserScore.jsx
+++ src/components/SubmitUserScore.jsx
@@ -1,9 +1,11 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import axios from 'axios';
 import { UserContext } from '../helpers/context';
 
 const SubmitUserScore = ({ wpm, accuracy, closeModal }) => {
   const { user } = useContext(UserContext);
+
+  const [buttonText, setButtonText] = useState('SUBMIT SCORE');
 
   const data = {
     wpm: wpm,
@@ -11,6 +13,7 @@ const SubmitUserScore = ({ wpm, accuracy, closeModal }) => {
   };
 
   const postRequest = () => {
+    setButtonText('SUBMITTING...');
     axios
       .post(`http://localhost:3000/submit`, data, {
         headers: {
@@ -19,14 +22,16 @@ const SubmitUserScore = ({ wpm, accuracy, closeModal }) => {
       })
       .then((res) => {
         console.log('Success: user score submitted');
+        setButtonText('SCORE SUBMITTED ✅');
       })
       .catch((err) => {
+        setButtonText('ERROR: TRY AGAIN');
         console.log('Error has occurred');
         console.log(err);
       });
   };
 
-  return <button onClick={() => postRequest()}>SUBMIT SCORE</button>;
+  return <button onClick={() => postRequest()}>{buttonText}</button>;
 };
 
 export default SubmitUserScore;
