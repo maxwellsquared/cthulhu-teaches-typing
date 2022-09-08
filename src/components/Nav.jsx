33eb2312706b11d8364515da12e883eb859c861b
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-09-08 10:44:03 -0700

    Nav link css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,14 +17,11 @@ const Nav = () => {
     <nav className="rounded px-2 py-2.5 font-normal text-pale-gold">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
         <div className="logo-text">
-        <Link to="/" className="flex items-center hover:text-candle">
-          <img
-            src="./images/logo.png"
-            className="mr-3 h-20 sm:h-20"
-            alt="logo"
-          />
-          <span className="logo-drip">CTHULHU</span> <span className="logo-lite">&nbsp;TEACHES TYPING</span>
-        </Link>
+          <Link to="/" className="flex items-center hover:text-candle">
+            <img src="./images/logo.png" className="mr-3 h-20 sm:h-20" alt="logo" />
+            <span className="logo-drip">CTHULHU</span>{' '}
+            <span className="logo-lite">&nbsp;TEACHES TYPING</span>
+          </Link>
         </div>
         <ul className="mt-0 flex flex-row p-4 font-light">
           {user ? (
@@ -42,7 +39,7 @@ const Nav = () => {
               <li>
                 <Link
                   to="/"
-                  className="mx-3 rounded-lg bg-pale-gold p-0 py-1 px-6  text-black"
+                  className="mx-3 rounded-lg bg-pale-gold p-0 py-1 px-6 text-black"
                   aria-current="logout"
                   onClick={refreshPage}
                 >
@@ -55,7 +52,7 @@ const Nav = () => {
               <li>
                 <Link
                   to="/login"
-                  className="mx-3 rounded-lg bg-pale-gold p-0 py-1 px-6  text-black"
+                  className="mx-3 rounded-lg bg-pale-gold p-0 py-1 px-6 text-lg text-cosmic-purple shadow-lg hover:bg-gold-hover"
                   aria-current="about"
                 >
                   LOGIN
@@ -64,7 +61,7 @@ const Nav = () => {
               <li>
                 <Link
                   to="/login"
-                  className="mx-3 rounded-lg bg-pale-gold p-0 py-1 px-4 text-black"
+                  className="mx-3 rounded-lg bg-pale-gold p-0 py-1 px-6 text-lg text-cosmic-purple shadow-lg hover:bg-gold-hover"
                   aria-current="about"
                 >
                   SIGNUP
