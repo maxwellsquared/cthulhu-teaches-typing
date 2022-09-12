1f54f0338056918eba1b1cc524eb677f5286ae51
Adam <adamgrharvey@gmail.com>
2022-09-12 13:21:13 -0700

    adds keyboard columns for type and colors

--- backend/typing/db/schema.rb
+++ backend/typing/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_173107) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_201404) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_173107) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "keyboard_type"
+    t.string "color1"
+    t.string "color2"
     t.index ["user_id"], name: "index_keyboards_on_user_id"
   end
 
